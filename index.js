c7d9a56bdbf07be38ccdc1f9fafffcee11cb16c0
Chihiro Saito <csaito1027@gmail.com>
2017-12-27 21:17:06 +0900

    fix bugs

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP   || '0.0.0.0';
 
-app.get('/v1/hello', (req, res) -> {
+app.get('/v1/hello', function(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.send('Hello World!\n');
 });
 
-app.listen(port, ip, () -> {
+app.listen(port, ip, function() {
 	console.log('Example app listening on port 3000!');
 });
 
